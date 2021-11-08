943d3312af7db2e1ef192f68990f2d75ecbe81e9
unknown <vladislav.tishchenko@mail.ru>
2021-11-08 18:06:38 +0300

    Первоначальное размещение проекта

---
+++ PZ_3/PZ_3_1.py
@@ -0,0 +1,41 @@
+#Дано трехзначное число. Проверить истинность высказывания: «Цифры данного числа
+#образуют возрастающую или убывающую последовательность».
+
+import sys
+
+ches1 = input("Введите первую цифру: ") # Вводим цифры
+ches2 = input("Введите вторую цифру: ")
+ches3 = input("Введите третью цифру: ")
+
+# Проверка
+while True:
+    try:
+        ches1 = int(ches1)
+        ches2 = int(ches2)
+        ches3 = int(ches3)
+        break
+    except ValueError:
+        print("Ошибка")
+        ches1 = input("Введите первую цифру: ")
+        ches2 = input("Введите вторую цифру: ")
+        ches3 = input("Введите третью цифру: ")
+
+# Проверяем условия и выводим ответ
+while True:
+    if 0 < ches1 < 10:
+        pass
+    elif 0 < ches2 < 10:
+        pass
+    elif 0 < ches3 < 10:
+        pass
+    else:
+        print("Ошибка")
+        sys.exit()
+    break
+
+if ches1 > ches2 > ches3:
+    print(True)
+elif ches1 < ches2 < ches3:
+    print(True)
+else:
+    print(False)
